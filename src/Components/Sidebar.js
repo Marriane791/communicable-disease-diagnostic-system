79e220d2390a584a5f6085ebb6e419b3dc2ccb29
Marriane791 <annemarrieakeyo42@gmail.com>
2022-03-23 00:12:14 +0300

    sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import MedicationIcon from '@mui/icons-material/Medication';
+import {sidebarItems} from '../data/SidebarData'
 
 
 function Sidebar() {
@@ -12,6 +13,19 @@ function Sidebar() {
             </IconMed>
             <Text>Diagnostic System</Text>
         </Top>
+        <Channels>
+            {
+                sidebarItems.map(item => (
+                    <ChannelItems>
+                        {item.icon}
+                        {item.text}
+                    </ChannelItems>
+                ))
+            }
+        </Channels>
+        <SidebarImg>
+            <img src='/images/kit2.jpg' />
+        </SidebarImg>
     </Main>
   )
 }
@@ -22,14 +36,36 @@ const Main = styled.div`
 color:#fff;
 `
 const Top = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
 padding-top:25px;
 `
 const IconMed = styled.div`
-padding-left:75px;
 `
 
 const Text = styled.div`
 font-size:12px;
 font-weight:700;
-padding-left:45px;
+`
+const Channels = styled.div`
+
+`
+const ChannelItems = styled.div`
+display:grid;
+grid-template-columns:15% auto;
+height:28px;
+align-items:center;
+padding-left:19px;
+padding-top:40px;
+cursor:pointer;
+font-size:12px;
+
+`
+const SidebarImg = styled.div`
+width:90px;
+height:90px;
+img {
+    width:100%;
+}
 `
---
+++ src/data/SidebarData.js
@@ -0,0 +1,29 @@
+import VaccinesIcon from '@mui/icons-material/Vaccines';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import SwitchAccountIcon from '@mui/icons-material/SwitchAccount';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+
+
+export  const sidebarItems =[
+    {
+       icon: <VaccinesIcon/>,
+       text: "Predict"
+    },
+
+    {
+        icon: <ChatBubbleOutlineIcon/>,
+        text: "Chat"
+     },
+
+     {
+        icon: <SwitchAccountIcon/>,
+        text: "Account"
+     },
+
+     {
+        icon: <SettingsOutlinedIcon/>,
+        text: "Settings"
+     }
+
+
+]
